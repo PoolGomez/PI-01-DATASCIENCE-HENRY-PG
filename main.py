9244e371bd6ab623d3a15bf0376a27d94fb47d46
paul479 <pgomez4790@gmail.com>
2023-03-16 16:44:38 -0500

    commit 2

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import pandas as pd
+#from numpy import numpy
+
+
+#titles = pd.read_csv(r"titles.csv")
+titles = pd.read_csv("https://github.com/PoolGomez/PI-01-DATASCIENCE-HENRY-PG/blob/main/datasets/titles.csv")
+app = FastAPI()
+    
+#http://127.0.0.1:8000
+@app.get("/")
+def index():
+    return {"mensaje":"Hello word"}
+
+##Cantidad de veces que aparece una keyword en el título de peliculas/series, por plataforma
+@app.get('/keyword/{plataforma}/{keyword}') #Segunda ruta
+def get_word_count(plataforma, keyword):
+    canal= titles[titles['id'].str.contains(plataforma[0], case= False)]
+    cantidad= canal[canal['title'].str.contains(keyword, case= False)]
+    return {'plataforma':plataforma, 'cantidad':len(cantidad)}
